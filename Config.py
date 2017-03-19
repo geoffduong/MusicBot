8ec05e4251e866cf1e2abaaffae58824c528e711
mrricearoni <duongg4848@gmail.com>
2017-03-19 18:21:22 -0400

    Cleaning up code Added Config.py to get rid of arg parsing Moved formatting of Reddit post to a method

---
+++ Config.py
@@ -0,0 +1,9 @@
+# Login info
+username = 'username'
+password = 'password'
+client_id = 'client_id'
+client_secret = 'client_secret'
+
+# Search parameters
+limit = 'limit'
+artistId = 'artistId'
--- submitArtistRecentCollection.py
+++ submitArtistRecentCollection.py
@@ -1,65 +1,76 @@
 import praw
 import json
-import sqlite3
+import Config
 from urllib2 import urlopen
 from sys import argv
 
-user_password = argv[1]
-artistId = argv[2]
-limit = argv[3]
-
-# Create UrlPath for iTunes API
-print 'Searching...'
-urlpath = urlopen('https://itunes.apple.com/lookup?id=' + artistId
-        + '&entity=album&limit=' + str(limit) + '&sort=recent')
-# Store JSON into result for later use and get Artist's name
-result = json.loads(urlpath.read())
-artistName = result['results'][0]['artistName']
-print 'Artist name entered: ' + artistName
-# Write to JSON file (strictly for testing purposes)
-target = open(artistName + '.json', 'w')
-target.truncate()
-print 'Writing to JSON file'
-urlpath = urlopen('https://itunes.apple.com/lookup?id=' + artistId
-        + '&entity=album&limit=' + str(limit) + '&sort=recent')
-target.write(urlpath.read())
-target.close()
-print 'Done writing to JSON file!'
-
-# Format information into tableResults for Reddit submission
-tableResults = ('{} | {} | {}\n' + \
-                '{} | {} | {}\n').format('Collection', 'Artist', 'iTunes Url',
-                                         '---', '---', '---')
-for i in result['results']:
-    if i['wrapperType'] == 'collection':
-        if 'contentAdvisoryRating' in i:
-            if i['artistName'] == artistName:
-                tableResults += ('{} ({}) | **{}** | [Link]({})\n').format(
-                        i['collectionName'], i['contentAdvisoryRating'],
-                        i['artistName'], i['collectionViewUrl']
-                )
+# Format results into Reddit format
+def formatResults(result):
+    tableResults = ('{} | {} | {}\n' + \
+                    '{} | {} | {}\n').format('Collection', 'Artist', 'iTunes Url',
+                                             '---', '---', '---')
+    for i in result['results']:
+        if i['wrapperType'] == 'collection':
+            if 'contentAdvisoryRating' in i:
+                if i['artistName'] == artistName:
+                    tableResults += ('{} ({}) | **{}** | [Link]({})\n').format(
+                            i['collectionName'], i['contentAdvisoryRating'],
+                            i['artistName'], i['collectionViewUrl'])
+                else:
+                    tableResults += ('{} ({}) | *{}* | [Link]({})\n').format(
+                            i['collectionName'], i['contentAdvisoryRating'],
+                            i['artistName'], i['collectionViewUrl'])
             else:
-                tableResults += ('{} ({}) | *{}* | [Link]({})\n').format(
-                        i['collectionName'], i['contentAdvisoryRating'],
-                        i['artistName'], i['collectionViewUrl']
-                )
-        else:
-            if i['artistName'] == artistName:
-                tableResults += ('{} (Clean) | **{}** | [Link]({})\n').format(
-                        i['collectionName'], i['artistName'],
-                        i['collectionViewUrl']
-                )
-            else:
-                tableResults += ('{} (Clean) | *{}* | [Link]({})\n').format(
-                        i['collectionName'], i['artistName'],
-                        i['collectionViewUrl']
-                )
+                if i['artistName'] == artistName:
+                    tableResults += ('{} (Clean) | **{}** | [Link]({})\n').format(
+                            i['collectionName'], i['artistName'],
+                            i['collectionViewUrl'])
+                else:
+                    tableResults += ('{} (Clean) | *{}* | [Link]({})\n').format(
+                            i['collectionName'], i['artistName'],
+                            i['collectionViewUrl'])
+    return tableResults
 
-""" Reddit submission here """
-titleText = artistName + '\'s ' + str(limit) +' Most Recent iTunes Albums'
-reddit = praw.Reddit(client_id = '',
-        client_secret = '',
-        password = user_password,
-        user_agent = 'asdf',
-        username = '')
-reddit.subreddit('').submit(title = titleText, selftext = tableResults)
+# Main method
+if __name__ == "__main__":
+    # Get args
+    artistId = Config.artistId
+    limit = Config.limit
+
+    # Create UrlPath for iTunes API
+    print 'Searching...'
+    urlpath = urlopen('https://itunes.apple.com/lookup?id=' + artistId
+            + '&entity=album&limit=' + limit + '&sort=recent')
+    # Store JSON into result for later use and get Artist's name
+    result = json.loads(urlpath.read())
+    artistName = result['results'][0]['artistName']
+    print 'Artist name entered: ' + artistName
+    # Write to JSON file (strictly for testing purposes)
+    target = open(artistName + '.json', 'w')
+    target.truncate()
+    print 'Writing to JSON file'
+    urlpath = urlopen('https://itunes.apple.com/lookup?id=' + artistId
+            + '&entity=album&limit=' + limit + '&sort=recent')
+    target.write(urlpath.read())
+    target.close()
+    print 'Done writing to JSON file!'
+
+    # Format information into tableResults for Reddit submission
+    tableResults = formatResults(result)
+    print tableResults
+
+
+    """ Reddit submission here """
+    titleText = artistName + '\'s ' + limit +' Most Recent iTunes Albums'
+    reddit = praw.Reddit(client_id = Config.client_id,
+            client_secret = Config.client_secret,
+            password = Config.password,
+            user_agent = 'asdf',
+            username = Config.username)
+    reddit.subreddit('test').submit(title = titleText, selftext = tableResults)
+
+    # tableResults += ("{0} " + ("({1})" if 'contentAdvisoryRating' in i else "(Clean)") + \
+    #         (" | **{2}**" if i['artistName'] == artistName else " | *{2}*") + \
+    #         " | [Link]({3})\n").format(
+    #                 i['collectionName'], i['contentAdvisoryRating'],
+    #                 i['artistName'], i['collectionViewUrl'])

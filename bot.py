8e3de211adb77693e69103318e59da988c554f67
mrricearoni <duongg4848@gmail.com>
2017-05-12 16:08:35 -0400

    Subreddit testing

--- bot.py
+++ bot.py
@@ -18,37 +18,32 @@ def main():
     subreddits = reddit.subreddit("+".join(Config.subreddits))
     print subreddits
 
-    # Testing bot
-    thread = "https://www.reddit.com/r/hiphopheads/comments/6ae0d3/fresh_quavo_paper_over_here/"
-    submission = reddit.submission(url=thread)
-    # comments = submission.comments.replace_more(limit=0)
-    submission.comments.replace_more(limit=0)
-    for comment in submission.comments.list():
-        text = comment.body
-        text = text.lower()
+    # TESTING BOT HERE
+    # Testing on single thread-------------------------------------------------
+    # thread = "https://www.reddit.com/r/hiphopheads/comments/6ae0d3/fresh_quavo_paper_over_here/"
+    # submission = reddit.submission(url=thread)
+    # submission.comments.replace_more(limit=0)
+    # for comment in submission.comments.list():
+    #     text = comment.body
+    #     text = text.lower()
+    #     comment_parsed = text.split(" ")
+    #     for word in comment_parsed:
+    #         if word in QUESTIONS:
+    #             reply_text = REPLY_TEMPLATE.format("https://soundcloud.com/ogxparker/quavo-paper-over-here-prod-og-parker#t=0:00")
+    #             comment.reply(reply_text)
+    #             break
+    # -------------------------------------------------------------------------
+
+    # Testing on a subreddit---------------------------------------------------
+    for comment in subreddits.stream.comments():
+        text = comment.body.lower()
         comment_parsed = text.split(" ")
         for word in comment_parsed:
             if word in QUESTIONS:
-                reply_text = REPLY_TEMPLATE.format("https://soundcloud.com/ogxparker/quavo-paper-over-here-prod-og-parker#t=0:00")
-                comment.reply(reply_text)
-                break
-        # print comment_parsed
-        # for word in comment_parsed:
-        #     if word in QUESTIONS:
-                # reply_text = REPLY_TEMPLATE.format("https://soundcloud.com/ogxparker/quavo-paper-over-here-prod-og-parker#t=0:00")
-                # comment.reply(reply_text)
-        #     else:
-        #         print False
-        # if comment.body in QUESTIONS:
-            # reply_text = REPLY_TEMPLATE.format("https://soundcloud.com/ogxparker/quavo-paper-over-here-prod-og-parker#t=0:00")
-            # comment.reply(reply_text)
-
-    # for comment in submission.comments:
-    #     print comment.body
-
-    # for comment in reddit.subreddit('test').stream.comments():
-    #     print comment.body
-
+                print text, comment.submission.shortlink
+        # print comment.body, comment.submission.shortlink
+    # -------------------------------------------------------------------------
+    
 # Main method
 if __name__ == "__main__":
     main()
